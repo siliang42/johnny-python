460e9f516035e4eedbd16b200d01e433dbda1a8c
jiayi.nie <jiayi.nie@baozun.com>
2018-12-13 17:05:48 +0800

    Initial commit

---
+++ venv/Include/com/jiayi/jiayi02.py
@@ -0,0 +1,19 @@
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >= 60:
+            return 'B'
+        else:
+            return 'C'
+
+bart = Student('Bart Simpson', 59)
+
+if __name__ =='__main__':
+    print(bart.name)
+
+
---
+++ venv/Include/com/jiayi/jiayi03.py
@@ -0,0 +1,23 @@
+class Animal(object):
+    def run(self):
+        print('Animal is running...')
+
+class Dog(Animal):
+    print('wangwangwang')
+    def run(self):
+        print('Dog is running...')
+
+    def eat(self):
+        print('Eating meat...')
+
+class Cat(Animal):
+    print('miaomiaomiao')
+
+    def run(self):
+        print('Cat is running...')
+
+if __name__=="__main__":
+    dog = Dog()
+    dog.run()
+    cat = Cat()
+    cat.run()
